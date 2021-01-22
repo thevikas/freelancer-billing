7d5fffda2f1c36c4940d2a82581076d82cbfacd2
Vikas Yadav <mevikas@gmail.com>
2021-01-22 15:48:19 +0530

    just navigating months

--- gt.php
+++ gt.php
@@ -1,6 +1,7 @@
 #!/usr/bin/env php
 <?php
 date_default_timezone_set('Asia/Kolkata');
+$all_lines = [];
 
 $logfile = getenv('HOME') . "/.gtimelog/timelog.txt";
 if (!file_exists($logfile)) {
@@ -10,42 +11,65 @@ if (!file_exists($logfile)) {
 $away=false;
 $argv2 = $argv;
 unset($argv2[0]);
+foreach($argv2 as $arg)
+{
+    if('-report' == $arg)
+    {
+        return makeReport();
+    }
+}
 $fullarg = implode(" ",$argv2);
 #Reading
 $L = fopen($logfile,"r");
 fseek($L,-200,SEEK_END);
 $last_dt = $last_time = $lc = 0;
-while(!feof($L))
+iterate($L);
+function iterate($L,$only_first = false,$callback = null)
 {
-    $line = trim(fgets($L));
-    $lc++;
-    if(!empty($line))
+    global $last_time;
+    global $last_comment;
+    global $all_lines;
+    global $lc;
+    while(!feof($L))
     {
-        //echo "$lc: $line\n";
-        $ss = explode(' ',$line,3);
-        if(count($ss)<3)
-            continue;
-        list($last_dt,$comment) = $ss;
-
-        $ss = explode(': ',$line,2);
-        if(count($ss)<2)
-            continue;
-        $last_time = strtotime($ss[0]);
-        $last_comment = $ss[1];
-
-        if(!empty($argv[1]))
+        $line = trim(fgets($L));
+        $lc++;
+        if(!empty($line))
         {
-            if("last" == $argv[1])
-                $fullarg = $last_comment;
-            else if("away" == $argv[1])
-            {
-                $away = true;
-                $fullarg = $last_comment;
-            }
+            //echo "$lc: $line\n";
+            $ss = explode(' ',$line,3);
+            if(count($ss)<3)
+                continue;
+            list($last_dt,$comment) = $ss;
+
+            $ss = explode(': ',$line,2);
+            if(count($ss)<2)
+                continue;
+            if(strlen($ss[0]) != 16)
+                continue;
+            $last_time = strtotime($ss[0]);
+            $last_comment = $ss[1];
+            $all_lines[$last_time] = [$last_time,$ss[0],$last_comment];
+            if($only_first)
+                break;
+            if(is_callable($callback) && call_user_func($callback,$all_lines[$last_time]))
+                break;
         }
     }
+    return $all_lines[$last_time];
 }
 fclose($L);
+
+if(!empty($argv[1]))
+{
+    if("last" == $argv[1])
+        $fullarg = $last_comment;
+    else if("away" == $argv[1])
+    {
+        $away = true;
+        $fullarg = $last_comment;
+    }
+}
 
 function difftime()
 {
@@ -84,3 +108,45 @@ $newline = sprintf("%s: %s",date('Y-m-d H:i'),$fullarg);
 fputs($L,$newline . "\n");
 echo difftime() . ": $fullarg\n";
 fclose($L);
+
+function checkIfItsThisMonth($info)
+{
+    $firstDay = strtotime(date('Y-m-01'));
+    list($tt,$tt2,$cc) = $info;
+    return $firstDay < $tt;
+}
+
+function find_start_of_last_month($L)
+{
+    fseek($L,-200,SEEK_CUR);
+    //find first line
+    $info = iterate($L,true);
+    $firstDay = strtotime(date('Y-m-01'));
+    $twoMonthsBack = strtotime("-3 months");
+    if($firstDay < $info[0])
+        return find_start_of_last_month($L);
+    return $info;
+    //check month
+    //if
+}
+
+function find_start_of_this_month($L)
+{
+    $info = iterate($L,false,"checkIfItsThisMonth");
+    return $info;
+    //check month
+    //if
+}
+
+function makeReport()
+{
+    global $all_lines,$logfile;
+
+    $L = fopen($logfile,"r");
+    fseek($L,-200,SEEK_END);
+    $info = find_start_of_last_month($L);
+    $info = find_start_of_this_month($L);
+    fseek($L,-(strlen($info[1] + $info[2] + 5)),SEEK_CUR);
+    iterate($L);
+
+}
