3eb8f6c85374a6e3ad065df0214a54b2799c8989
Vikas Yadav <mevikas@gmail.com>
2022-03-01 17:08:58 +0530

    added invoice sorting by dates and invoice numbers

---
+++ views/bills/_form.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Bill */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="bill-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id_client')->textInput() ?>
+
+    <?= $form->field($model, 'dated')->textInput() ?>
+
+    <?= $form->field($model, 'hours')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
