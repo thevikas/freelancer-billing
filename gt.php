6cb43b1e4bd3943a29724f013a53337aa7000513
thevikas <94922+thevikas@users.noreply.github.com>
2023-01-29 16:14:26 +0530

    Creating bills for certain project wip

--- gt.php
+++ gt.php
@@ -1,15 +1,20 @@
 #!/usr/bin/env php
 <?php
+
+namespace gtimelogphp;
+
 require __DIR__ . "/vendor/autoload.php";
 require_once 'functions.php';
 
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 $logfile = getenv('HOME') . '/.local/share/gtimelog/timelog.txt';
 
-$hello_cmd = new Commando\Command();
+$hello_cmd = new \Commando\Command();
 
 $hello_cmd->option()
     ->describedAs('Log entry');
-
 
 // Define a boolean flag "-c" aka "--capitalize"
 $hello_cmd->option('r')
@@ -24,12 +29,22 @@ $hello_cmd->option('u')
 
 $hello_cmd->option('b')
     ->aka('bill')
-    ->describedAs('Bill')
+    ->describedAs('Generate monthly bill report')
     ->boolean();
 
 $hello_cmd->option('e')
     ->aka('earning')
     ->describedAs('Just report this months earnings')
+    ->boolean();
+
+$hello_cmd->option('p')
+    ->aka('project')
+    ->describedAs('Generate monthly bill json')
+    ->boolean();
+
+$hello_cmd->option('i')
+    ->aka('inum')
+    ->describedAs('Get next invoice num')
     ->boolean();
 
 // Define a boolean flag "-c" aka "--capitalize"
@@ -41,37 +56,62 @@ $hello_cmd->option('m')
 date_default_timezone_set('Asia/Kolkata');
 $all_lines = [];
 
-if (!file_exists($logfile)) {
+if (!file_exists($logfile))
+{
     fputs(STDERR, "$logfile: File not found\n");
     return -1;
 }
-$away  = false;
+$away = false;
 $argv2 = $argv;
 
-if ($hello_cmd['report'] || $hello_cmd['bill'] || $hello_cmd['earning']) {
-    $rep = new gtimelogphp\MonthReport($logfile);
+if ($hello_cmd['inum'])
+{
+    $num = Bill::getNextInvoiceNumber();
+    echo "Next invoice num:" . $num;
+    return 0;
+}
+
+if ($hello_cmd['report'] || $hello_cmd['bill'] || $hello_cmd['earning'])
+{
+    $rep = new MonthReport($logfile);
     if ('last_month' == $hello_cmd['month'])
-        $FirstDayOfMonth      = strtotime(date('Y-m-01', strtotime("-1 month")));
+    {
+        $FirstDayOfMonth = strtotime(date('Y-m-01', strtotime("-1 month")));
+    }
     else if ('this_month' == $hello_cmd['month'])
-        $FirstDayOfMonth      = strtotime(date('Y-m-01'));
+    {
+        $FirstDayOfMonth = strtotime(date('Y-m-01'));
+    }
     else
-        $FirstDayOfMonth      = strtotime(date('Y-m-01', strtotime($hello_cmd['month'])));
+    {
+        $FirstDayOfMonth = strtotime(date('Y-m-01', strtotime($hello_cmd['month'])));
+    }
+
     #echo "FirstDayOfMonth = " . date('Y-m-d',$FirstDayOfMonth) . "\n";
     $report_data = $rep->report($FirstDayOfMonth);
-    if ($hello_cmd['report']) {
+    if ($hello_cmd['report'])
+    {
         print_r($report_data);
-    } else if ($hello_cmd['bill'] || $hello_cmd['earning']) {
-        $bill = new gtimelogphp\Bill($report_data);
+    }
+    else if ($hello_cmd['bill'] || $hello_cmd['earning'])
+    {
+        $bill = new Bill($report_data);
         $rep = $bill->report();
         if ($hello_cmd['earning'])
+        {
             echo sprintf("%d", round($rep['TotalEarning'])) . "\n";
+        }
         else
-            print_r($rep);;
+        {
+            print_r($rep);
+        }
+
     }
     return;
 }
 
-if ($hello_cmd['undo']) {
+if ($hello_cmd['undo'])
+{
     undo($logfile);
     return;
 }
@@ -85,16 +125,21 @@ iterate($L);
 
 fclose($L);
 
-if (!empty($argv[1])) {
-    if ('last' == $argv[1]) {
+if (!empty($argv[1]))
+{
+    if ('last' == $argv[1])
+    {
         $fullarg = $last_comment;
-    } else if ('away' == $argv[1]) {
-        $away    = true;
+    }
+    else if ('away' == $argv[1])
+    {
+        $away = true;
         $fullarg = $last_comment;
     }
 }
 
-if (empty($argv[1])) //if not arg given, we just show time spent doing the last item
+//if not arg given, we just show time spent doing the last item
+if (empty($argv[1]))
 {
     $mm = difftime();
     echo "$mm: $last_comment\n";
@@ -105,15 +150,17 @@ if (empty($argv[1])) //if not arg given, we just show time spent doing the last 
 $L = fopen($logfile, 'a');
 #fseek($L, -200, SEEK_END);
 
-$last_dt    = date('Y-m-d', $last_time);
+$last_dt = date('Y-m-d', $last_time);
 $today_date = date('Y-m-d');
-if (strstr($last_dt, $today_date) === false) //Not same day
+//Not same day
+if (strstr($last_dt, $today_date) === false)
 {
     fputs($L, "\n");
 }
 
 //mark a time period as away between last and this and resume the work
-if ($away) {
+if ($away)
+{
     $newline = sprintf('%s: away **', date('Y-m-d H:i'));
     fputs($L, $newline . "\n");
 }
