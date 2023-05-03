4c851f70d0c5e9bef74a4da6f039b8aa3c45fe7a
Vikas Yadav <mevikas@gmail.com>
2023-05-03 11:51:04 +0530

    made skeleton to work with a project controller

---
+++ views/projects/_form.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Project */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="project-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
