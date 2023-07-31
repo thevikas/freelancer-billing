ad5eeabdd09e9b79b70d83c2495086339423c986
Vikas Yadav <mevikas@gmail.com>
2023-07-31 21:20:06 +0530

    week report also made

--- Project.php
+++ Project.php
@@ -15,6 +15,8 @@ class Project
      */
     public $task_times;
 
+    public $task_week_times;
+
     /**
      * @var mixed
      */
@@ -28,6 +30,18 @@ class Project
         $this->name = $name;
     }
 
+    function getDateTimeFromWeekNumber($weekNumber, $year) {
+        // Create a new DateTime object for the first day of the year
+        $dateTime = new \DateTime();
+        $dateTime->setISODate($year, $weekNumber, 1); // Set to the first day of the specified week
+        
+        // Calculate the number of days to add to get to the desired week
+        //$daysToAdd = ($weekNumber - 1) * 7;
+        //$dateTime->add(new \DateInterval("P{$daysToAdd}D"));
+        //convert to string
+        return $dateTime->format('Y-m-d');
+    }
+
     /**
      * @param $info
      * @param $spent_time_secs
@@ -39,7 +53,11 @@ class Project
         {
             $this->task_times[$task] = 0;
         }
+        $week = date('W',$ts = strtotime($info['given_ts']));
+        $year = date('Y',$ts = strtotime($info['given_ts']));
+        $weekDateTime = $this->getDateTimeFromWeekNumber($week, $year);
 
+        $this->task_week_times[$weekDateTime][$task] += $spent_time_secs;
         $this->task_times[$task] += $spent_time_secs;
         $this->last_datetime = $tasktime;
     }
@@ -56,6 +74,7 @@ class Project
         }
         list($hours,$mins) = explode(':',getHourMins($total));
         $hours += $mins/60;
+        $rep['Weeks'] = $this->task_week_times;
         $rep['Total'] = $hours;
         $rep['Dated'] = $this->last_datetime;
         return $rep;
