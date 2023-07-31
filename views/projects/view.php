e00f6b0767a3892bb1a9dc363eb50f107a97fea6
Vikas Yadav <mevikas@gmail.com>
2023-07-31 23:14:08 +0530

    grouping the timesheet display for all weeks

--- views/projects/view.php
+++ views/projects/view.php
@@ -12,24 +12,63 @@ $this->title = $model->name;
 $this->params['breadcrumbs'][] = ['label' => 'Projects', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 */
+
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="project-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php 
-        echo $this->render("_proj_header",['proj' => $proj,'projcode' => $projcode]); 
+    <?php
+    echo $this->render("_proj_header", ['proj' => $proj, 'projcode' => $projcode]);
     ?>
 
-<?=GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns'      => [
-        ['class' => 'yii\grid\SerialColumn'],
-        'task',
-        'spent',
-        'times',
-    ],
-]);?>
-    
+    <ul class="nav nav-tabs" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="home-tab" data-bs-toggle="tab" href="#month" role="tab" aria-controls="home" aria-selected="true">Month</a>
+        </li>
+        <?php
+        foreach ($weeks as $n => $weekdate)
+        {
+        ?>
+            <li class="nav-item" role="presentation">
+                <a class="nav-link" id="week<?= $n ?>-tab" data-bs-toggle="tab" data-bs-target="#week<?= $n ?>" href="#week<?= $n ?>" role="tab" aria-controls="week<?= $n ?>" aria-selected="false"><?= $weekdate ?></a>
+            </li>
+        <?php
+        }
+        ?>
+    </ul>
+
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="month" role="tabpanel" aria-labelledby="month-tab" tabindex="0">
+            <?= GridView::widget([
+                'dataProvider' => $dataProviderAll,
+                'columns'      => [
+                    ['class' => 'yii\grid\SerialColumn'],
+                    'task',
+                    'spent',
+                    'times',
+                ],
+            ]); ?>
+        </div>
+        <?php
+        foreach ($weeks as $n => $weekdate)
+        {
+        ?>
+            <div class="tab-pane fade" id="week<?= $n ?>" role="tabpanel" aria-labelledby="week<?= $n ?>-tab">
+                <?= GridView::widget([
+                    'dataProvider' => $dataProviderWeeks[$n],
+                    'columns'      => [
+                        ['class' => 'yii\grid\SerialColumn'],
+                        'task',
+                        'spent',
+                        'times',
+                    ],
+                ]); ?>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
+
 </div>
